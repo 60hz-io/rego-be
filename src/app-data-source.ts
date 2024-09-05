6e655f9d9d27d51811893d22bdd755e20d921b06
sw-yu-60hz <sw.yu@60hz.io>
2024-09-05 18:23:53 +0900

    빌드 스크립트에 따라서 다른 db를 바라보도록 환경변수를 설정합니다.

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,23 +1,23 @@
-import oracledb from 'oracledb';
+import oracledb from "oracledb";
 
 oracledb.initOracleClient({
-  libDir: './oracle-client',
+  libDir: "./oracle-client",
 });
 
-const CONNECTION_POOL_ALIAS = 'pool';
+const CONNECTION_POOL_ALIAS = "pool";
 
 export const pool = oracledb
   .createPool({
-    user: 'REGO',
-    password: 'rego!!814',
-    connectString: '172.16.5.37:1525',
-    poolMax: 20, // 최대 연결 수
-    poolMin: 5, // 최소 연결 수
-    queueTimeout: 60000, // 큐 타임아웃 설정,
+    user: process.env.ORACLE_USER,
+    password: process.env.ORACLE_PASSWORD,
+    connectString: process.env.ORACLE_CONNECT_STRING,
+    poolMin: 5,
+    poolMax: 20,
+    queueTimeout: 60000,
     poolAlias: CONNECTION_POOL_ALIAS,
   })
-  .then(() => console.log('database connection is success'))
-  .catch(() => console.log('database connection error'));
+  .then(() => console.log("database connection is success"))
+  .catch(() => console.log("database connection error"));
 
 export const getConnection = async () => {
   return await oracledb.getConnection(CONNECTION_POOL_ALIAS);
